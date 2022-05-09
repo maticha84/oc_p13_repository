0107196b662169008b10d992cf1276a1a924d20c
maticha84 <maticha84@gmail.com>
2022-05-09 10:34:42 +0200

    refactor profiles templates/url/view

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,8 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls')),
-
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
