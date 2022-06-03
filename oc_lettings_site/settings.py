9d23cff9f5da237f95aa50e7cd60d8d6c16044c5
maticha84 <maticha84@gmail.com>
2022-06-03 08:57:24 +0200

    Intregration heroku in circle CI - modify dockerfile with gunicorn

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -8,12 +8,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+if os.environ.get('ENV') != 'PRODUCTION':
+    SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+    DEBUG = True
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'oc-lettings-1984.herokuapp.com']
 
 
 # Application definition
