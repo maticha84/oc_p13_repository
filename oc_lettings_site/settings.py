c69a597e18212eaf6e19fcd826cbad0a57c34908
maticha84 <maticha84@gmail.com>
2022-06-03 09:22:27 +0200

    CI/CD OK - TODO Sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,7 @@ else:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'oc-lettings-1984.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
