4c2c8bcf8360afa69fdd3c8a71c96d79c83561d3
maticha84 <maticha84@gmail.com>
2022-06-17 17:35:00 +0200

    Final commit

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 sentry_sdk.init(
     dsn=os.getenv('SENTRY_DNS'),
