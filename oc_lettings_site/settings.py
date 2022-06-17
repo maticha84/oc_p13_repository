10c6dc9d238aac0909f004bca0f8c36f14dfed4f
maticha84 <maticha84@gmail.com>
2022-06-17 18:18:10 +0200

    Static files debug false

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 sentry_sdk.init(
     dsn=os.getenv('SENTRY_DNS'),
