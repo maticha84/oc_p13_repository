e5cf693366f5ed75dbc1dfafaa8d880b3b44106f
maticha84 <maticha84@gmail.com>
2022-06-17 18:26:26 +0200

    Static files debug false

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/app/staticfiles"
 
 sentry_sdk.init(
     dsn=os.getenv('SENTRY_DNS'),
