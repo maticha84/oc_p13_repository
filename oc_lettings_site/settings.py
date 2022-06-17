a12cbbea725ec5df56858745adf8c79c75a919ee
maticha84 <maticha84@gmail.com>
2022-06-17 17:54:28 +0200

    Static files debug false

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,13 @@
 import os
+
 import django_on_heroku
+
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
